7bd3c42d83e6542137a02815443b0a273422c6ec
ituniti <inituniti@gmail.com>
2020-12-01 04:55:11 +0300

    refactor: html, css, js

--- script/index.js
+++ script/index.js
@@ -3,15 +3,9 @@ let popupNode = document.querySelector('.popup');
 let popupCloseButtonNode = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let formName = document.querySelector('.form__name');
-let formJob = document.querySelector('.form__job');
+let formName = document.querySelector('.form__input_name');
+let formJob = document.querySelector('.form__input_job');
 let formElement = document.querySelector('.form');
-
-
-
-
-profileInfoEdit.addEventListener('click', handleProfileInfoEditClick);
-popupCloseButtonNode.addEventListener('click', handlePopupCloseButtonClick);
 
 function handleProfileInfoEditClick() {
   popupNode.classList.add('popup_opened');
@@ -19,14 +13,19 @@ formName.value = profileName.textContent;
 formJob.value = profileJob.textContent;
 }
 
+profileInfoEdit.addEventListener('click', handleProfileInfoEditClick);
+
 function handlePopupCloseButtonClick() {
   popupNode.classList.remove('popup_opened');
 }
+
+popupCloseButtonNode.addEventListener('click', handlePopupCloseButtonClick);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     profileName.textContent = formName.value;
     profileJob.textContent = formJob.value;
+    popupNode.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
