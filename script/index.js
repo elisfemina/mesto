986bab33307b4ce0e2f3f1a9a6bababf90304216
ituniti <inituniti@gmail.com>
2020-12-14 05:27:19 +0300

    feat: make open close popup

--- script/index.js
+++ script/index.js
@@ -25,36 +25,57 @@ const initialCards = [
     }
 ];
 
-const profileInfoEdit = document.querySelector('.profile__info-edit');
-const popupNode = document.querySelector('.popup');
-const popupCloseButtonNode = document.querySelector('.popup__close-button');
+// Селекторы попапов
+
+const popupNode = document.querySelector('.popup'); //удалить
+const popupEditProfile = document.querySelector('.popup_act_edit-profile');
+const popupAddCard = document.querySelector('.popup_act_add-card');
+
+// Селектор кнопки открытия формы редактирования профиля
+const buttonInfoEdit = document.querySelector('.profile__info-edit');
+
+// Селектор кнопки открытия формы добавления места
+const buttonAddCard = document.querySelector('.add-button');
+
+// Селектор кнопки закрытия попапов
+const popupCloseButtonNode = document.querySelector('.popup__close-button'); //заменить
+const buttonCloseEditProfile = document.querySelector('.popup__close-button_edit-profile');
+const buttonCloseAddCard = document.querySelector('.popup__close-button_add-card');
+
+// Селектор полей формы редактирования профиля
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const formName = document.querySelector('.form__input_edit_name');
 const formJob = document.querySelector('.form__input_edit_job');
 const formElement = document.querySelector('.form');
 
-// Переменная кнопки добавления места
-const buttonAddPlace = document.querySelector('.add-button');
+// Селектор полей формы добавления карточки места
+const formInputCardTitle = document.querySelector('.form__input_edit_card-title');
+const formInputCardLinkImage = document.querySelector('.form__input_edit_card-link-image');
 
-// Переменные попапа ввода полей добавления места
-const formCardTitle = document.querySelector('.form__input_edit_card-title');
-const formCardLinkImage = document.querySelector('.form__input_edit_card-link-image');
+// Функция открытия формы
+function handleButtonClick(popup) {
+  popup.classList.add('popup_opened');
 
-// Попап редактирования профиля
-function handleProfileInfoEditClick() {
-  popupNode.classList.add('popup_opened');
+  if (popup === popupEditProfile) {
 formName.value = profileName.textContent;
 formJob.value = profileJob.textContent;
+  }
 }
+// Вызов функции открытия формы редактирования профиля
+buttonInfoEdit.addEventListener('click', popup => handleButtonClick(popupEditProfile));
 
-profileInfoEdit.addEventListener('click', handleProfileInfoEditClick);
+// Вызов функции открытия формы добавления карточки
+buttonAddCard.addEventListener('click', popup => handleButtonClick(popupAddCard));
 
-function handlePopupCloseButtonClick() {
-  popupNode.classList.remove('popup_opened');
+// Функция закрытия формы
+function handlePopupCloseButtonClick(popup) {
+  popup.classList.remove('popup_opened');
 }
+// Вызов функции закрытия формы
+buttonCloseEditProfile.addEventListener('click', popup => handlePopupCloseButtonClick(popupEditProfile));
 
-popupCloseButtonNode.addEventListener('click', handlePopupCloseButtonClick);
+buttonCloseAddCard.addEventListener('click', popup => handlePopupCloseButtonClick(popupAddCard));
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -75,4 +96,4 @@ formCardLinkImage.value = 'Ссылка на картинку';
 
 // Добавляем слушатель для кнопки добавления карточки места
 
-buttonAddPlace.addEventListener('click', handleCardPlaceEditClick);
+// buttonAddPlace.addEventListener('click', handleCardPlaceEditClick);
