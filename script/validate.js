4e419517a59df0a61c616ce75bc8364d37f9e41c
ituniti <inituniti@gmail.com>
2020-12-25 22:05:52 +0300

    feat: close popup js

--- script/validate.js
+++ script/validate.js
@@ -1,15 +1,18 @@
-function showError(form, input, config) {
+  // Отображение ошибки
+  function showError(form, input, config) {
   const error = form.querySelector(`#${input.id}-error`);
   error.textContent = input.validationMessage;
   input.classList.add(config.inputErrorClass);
 }
 
+// Cкрытие ошибки
 function hideError(form, input, config) {
   const error = form.querySelector(`#${input.id}-error`);
   error.textContent = "";
   input.classList.remove(config.inputErrorClass);
 }
 
+// Проверка валидности
 function checkInputValidity(form, input, config) {
   if (input.validity.valid) {
     hideError(form, input, config);
@@ -18,6 +21,7 @@ function checkInputValidity(form, input, config) {
   }
 }
 
+// Блокировка-разблокировка кнопки
 function setButtonState(button, isActive, config) {
   if (isActive) {
     button.classList.remove(config.inactiveButtonClass);
@@ -41,6 +45,7 @@ function setEventListener(form, config) {
   });
 }
 
+// Поиск нужной формы и кнопки
 function enableValidation(config) {
   const forms = document.querySelectorAll(config.formSelector);
   forms.forEach(form => {
@@ -55,13 +60,13 @@ function enableValidation(config) {
     setButtonState(submitButton, form.checkValidity(), config);
   });
 }
+
 const validationConfig = {
   formSelector: '.form',
   inputSelector: '.form__input',
   submitButtonSelector: '.form__button',
   inactiveButtonClass: 'form__button_invalid',
-  inputErrorClass: 'form__input_state_invalid',
-  errorClass: 'popup__error_visible'
-};
+  inputErrorClass: 'form__input_state_invalid'
+  };
 
 enableValidation(validationConfig);
